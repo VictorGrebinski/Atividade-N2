6696e624c77d002a564bbdcaa17d507244beee18
VictorGrebinski <victoralexandregrebinski@gmail.com>
2025-06-02 21:55:37 -0300

    criação do projeto
    criado a Main, modificado css, criado componentes, pages

--- cadastro-usuarios/release.config.js
+++ cadastro-usuarios/release.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   branches: ['main'], // Aqui é o nome da branch principal do seu repositório (ex: 'main' ou 'master')
   plugins: [
-    '@semantic-release/commit-analyzer',         // Lê os tipos de commits
+    '@semantic-release/commit-analyzer',         // Lê os tipos de commit
     '@semantic-release/release-notes-generator', // Gera as notas de versão
     '@semantic-release/changelog',               // Atualiza o CHANGELOG.md
     '@semantic-release/npm',                     // Atualiza a versão no package.json
